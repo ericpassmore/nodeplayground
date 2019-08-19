0919647af0cc131a826b157d7455e3d92eb1860b
Eric Passmore <eric.passmore@gmail.com>
2019-08-19 01:43:49 -0700

    updated added sleep to let page load, and write JSON to file

--- puppeteer/scrapeGrouponCareers.js
+++ puppeteer/scrapeGrouponCareers.js
@@ -11,20 +11,22 @@ const puppeteer = require('puppeteer-core');
   //await page.screenshot({path: 'frontpage.png'});
   await page.click('input#btnSearch');
 
-
   let moreResults = true;
   const jobs = [];
   let max = 20;
 
+  const fs = require('fs');
+
   while(moreResults){
 
+    await page.waitFor(800);
     await page.waitForSelector('td.JobTitle-cell');
     //await page.screenshot({path: 'careers'.concat( max ,'.png')});
 
     const newJobs = await page.evaluate(() => {
       const trs = Array.from(document.querySelectorAll('table tbody tr'));
 
-      return trs.slice(1).map(tr => {
+      return trs.map(tr => {
         const dataNodeList = tr.querySelectorAll('td');
         const dataArray = Array.from(dataNodeList);
         const jobId = dataArray[0].innerHTML.split("/")[3]
@@ -41,11 +43,16 @@ const puppeteer = require('puppeteer-core');
       max = max - 1;
       if (max <= 0 ) { moreResults = false };
     } catch(error) {
-      console.log(error);
+      //console.log(error);
       moreResults = false;
     }
   }
   //process.stdout.write(data);
-  console.log(jobs);
+  //console.log(jobs);
+  fs.writeFile(
+    './ouput.json',
+    JSON.stringify(jobs,null,2),
+    (err) => err ? console.log("not written") : console.log("written ok")
+  )
   await browser.close();
 })();
